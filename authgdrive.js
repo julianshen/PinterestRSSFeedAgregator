66fa2f7e885f51cbe5d27eb5f8fbbc48f8b56311
julianshen <julianshen@gmail.com>
2013-08-18 23:14:05 +0800

    add script to auth gclient

---
+++ authgdrive.js
@@ -0,0 +1,49 @@
+var googleapis = require('googleapis');
+var OAuth2Client = googleapis.OAuth2Client;
+var fs = require('fs');
+var config = require('./gclient_config');
+
+function getAccessToken(oauth2Client, callback) {
+    // generate consent page url
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/drive'
+    });
+
+    console.log('Visit the url: ', url);
+
+    var http = require('http');
+    var server = http.createServer(function(req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('OK\n');
+
+        var query = require('url').parse(req.url, true).query;
+        if (query.code != null) {
+            // request access token
+            oauth2Client.getToken(query.code, function(err, tokens) {
+                oauth2Client.credentials = tokens;
+                console.log(tokens);
+                callback(tokens);
+            });
+        }
+    });
+    server.listen(1888, '127.0.0.1');
+}
+
+var oauth2Client =
+    new OAuth2Client(config.CLIENT_ID, config.CLIENT_SECRET, config.REDIRECT_URL);
+
+// retrieve an access token
+getAccessToken(oauth2Client, function(tokens) {
+    console.log('Saving to ' + config.GOOGLE_TOKEN_FILE);
+
+    fs.writeFile(config.GOOGLE_TOKEN_FILE, JSON.stringify(tokens), function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('Token saved');
+        process.exit();
+    });
+});
---
+++ gclient_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    CLIENT_ID: 'PUT YOUR CLIENT ID HERE',
+    CLIENT_SECRET: 'PUT YOUR CLIENT SECRET HERE',
+    REDIRECT_URL: 'http://localhost:1888/oauth2callback',
+    GOOGLE_TOKEN_FILE: ".gtoken"
+}
